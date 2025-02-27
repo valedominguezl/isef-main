c60bab355f8b8ad07997a1001dcffaffa518cdaf
valedominguez <vx.dominguez@alumno.um.edu.ar>
2025-02-27 06:45:25 +0000

    node con Expressy  SSR

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    ssr: true, 
+    outDir: 'dist', 
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/entry-server.jsx')
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
