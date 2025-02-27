24deb1d9ed44f60dde4179f47f5500a5a31f5ee1
valedominguez <vx.dominguez@alumno.um.edu.ar>
2025-02-27 07:23:10 +0000

    m

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { StaticRouter } from 'react-router-dom/server';
 import { HelmetProvider } from 'react-helmet-async';
-import {App} from './main.jsx';
+import App from './main.jsx';
 
 export default function EntryServer({ url, context = {} }) {
   return (
