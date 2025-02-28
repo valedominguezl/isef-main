474fdbd8aa61060d269b1099682f9db11fdc1097
valedominguez <vx.dominguez@alumno.um.edu.ar>
2025-02-28 13:48:10 +0000

    M

--- src/components/home/novedades/novedadLista.js
+++ src/components/home/novedades/novedadLista.js
@@ -1,3 +1,5 @@
+import getCourseImage from '@/components/especializaciones/cursos/cursosData/getCourseImage';
+
 const novedadLista = [
   {
     title: 'Laboratorio de investigación',
@@ -5,9 +7,8 @@ const novedadLista = [
       'En este 2025 comenzará la primera etapa de nuestro laboratorio de anatomía funcional, fisiología del ejercicio y nutrición, a los efectos de promover y desarrollar investigaciones en las áreas afines. Será una nueva etapa en la historia de nuestra institución, que nos permitirá seguir creciendo y aportando al conocimiento científico.',
     page: '/Especializaciones',
     id: 'especializacionesCursos',
-    imagePath: '/src/assets/media/especializaciones/cursos/investigacion.webp',
+    imagePath: getCourseImage('investigacion.webp'),  
   },
-
 ];
 
 export default novedadLista;
